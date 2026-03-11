6de3beb5536d5953ae54554dd158fa9b43d2421e
Kosuke <mkosuke711@gmail.com>
2026-03-11 09:10:05 +0100

    Remove the edge case since not needed

--- LeetCode/leet476.cs
+++ LeetCode/leet476.cs
@@ -1,7 +1,8 @@
+using System.Numerics;
 public class Solution
 {
     public int FindComplement(int num)
     {
-        return num == 0 ? 1 : (1 << (BitOperations.Log2((uint)num) + 1)) - 1 - num;
+        return (1 << (BitOperations.Log2((uint)num) + 1)) - 1 - num;
     }
 }
