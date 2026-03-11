2e1893643c33e273667eade3a8ec00fa0834642e
Kosuke <mkosuke711@gmail.com>
2026-03-11 08:23:32 +0100

    Add an altanative sol for 1009

--- LeetCode/leet1009.cs
+++ LeetCode/leet1009.cs
@@ -7,3 +7,17 @@ public class Solution
         return n == 0 ? 1 : (1 << (BitOperations.Log2((uint)n) + 1)) - 1 - n;
     }
 }
+
+
+// Possible soltion 
+/*
+public class Solution 
+{ 
+    public int BitwiseComplement(int n) 
+    { 
+        int bits = 32 - BitOperations.LeadingZeroCount((uint)(n | 1));
+        int mask = (1 << bits) - 1;
+        return ~n & mask;
+    }
+}
+*/
