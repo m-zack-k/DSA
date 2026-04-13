e7b9585ac2e8b2193836aa9521fe8cba7a38ebc9
Kosuke <mkosuke711@gmail.com>
2026-04-13 19:05:57 +0200

    Add 1848 w/ 100% efficiency on Leetcode

---
+++ LeetCode/leet1848.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    public int GetMinDistance(int[] nums, int target, int start)
+    {
+        int n = nums.Length;
+
+        if (nums[start] == target)
+        {
+            return 0;
+        }
+        int left = start - 1;
+        int right = start + 1;
+
+        int leftRange = start;
+        int rightRange = n - 1 - start;
+
+        int d = 1;
+        while (true)
+        {
+            if (left >= 0 && nums[left] == target)
+            {
+                return start - left;
+            }
+
+            if (right < n && nums[right] == target)
+            {
+                return right - start;
+            }
+
+            left--;
+            right++;
+        }
+    }
+}
