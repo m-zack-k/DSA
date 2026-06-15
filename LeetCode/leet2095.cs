24278707ce0d2f955760596c074498faae80cb99
Kosuke <mkosuke711@gmail.com>
2026-06-16 00:05:55 +0200

    Add: 2095 sol

---
+++ LeetCode/leet2095.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        if (head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = slow.next.next;
+        while (fast != null && fast.next != null){
+            
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+
+    }
+}
